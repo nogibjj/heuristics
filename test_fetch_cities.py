cf18b2b202bd061adedceaad9527c50283aae4a6
Noah Gift <noah.gift@gmail.com>
2022-09-08 15:57:01 +0000

    adding a test

---
+++ test_fetch_cities.py
@@ -0,0 +1,20 @@
+from fetch_cities_lat_long import main, my_cities, create_cities_dataframe
+import pytest
+
+# create a list of cities
+city_list = my_cities("New York", "Knoxville", "Los Angeles", "Chicago")
+# create a dataframe of cities and their latitudes and longitudes
+df = create_cities_dataframe(city_list)
+
+def test_my_cities():
+    """Test my_cities function"""
+    assert my_cities("New York", "Knoxville", "Los Angeles", "Chicago") == [
+        "New York",
+        "Knoxville",
+        "Los Angeles",
+        "Chicago",
+    ]
+
+def test_main():
+    """Test main function"""
+    assert main(count=1) == None
