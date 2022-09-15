1db245574579078f3d48a8171058a2ee274e0264
Noah Gift <noah.gift@gmail.com>
2022-09-15 10:40:23 +0000

    adding translator

---
+++ translator.py
@@ -0,0 +1,15 @@
+# build a for loop in python
+for i in range(5):
+    print(i)
+
+# bash version
+#for i in {0..4}
+#do
+#    echo $i
+#done
+
+## csharp version
+#for (int i = 0; i < 5; i++)
+#{
+#    Console.WriteLine(i);
+#}
