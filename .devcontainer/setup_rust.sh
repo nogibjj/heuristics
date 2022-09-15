5ddece631065afeda87a4f0bffdea9bcc654fff0
Noah Gift <noah.gift@gmail.com>
2022-09-15 23:53:59 +0000

    push

--- .devcontainer/setup_rust.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Install Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-
-
-# setup path
-echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
